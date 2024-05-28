dd665109eba3be5438150dad7157922eaf7fb5aa
bency <bency80097@gmail.com>
2024-05-28 11:34:51 +0800

    chore: "Implement localization and project renaming updates"
    - Change the project name in `package-lock.json` from `traffic-light` to `traffic.keeping.work`
    - Add favicon link to the `create.blade.php` file in the `traffic-light` view
    - Translate the form labels and options in `create.blade.php` from English to Chinese
    
    Signed-off-by: bency <bency80097@gmail.com>

--- resources/views/traffic-light/create.blade.php
+++ resources/views/traffic-light/create.blade.php
@@ -5,30 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Traffic Light</title>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/traffic-light-create.js'])
 </head>
 
 <body>
     @include('layouts.navbar')
     <div class="container">
-        <h1 class="my-4">Create Traffic Light</h1>
+        <h1 class="my-4">建立新的紅綠燈時相</h1>
 
         <div class="mb-4">
-            <label for="county" class="form-label">County:</label>
+            <label for="county" class="form-label">縣市:</label>
             <select id="county" name="county" class="form-control">
-                <option value="">Select County</option>
+                <option value="">選擇縣市</option>
             </select>
         </div>
 
         <div class="mb-4">
-            <label for="district" class="form-label">District:</label>
+            <label for="district" class="form-label">鄉鎮市區:</label>
             <select id="district" name="district" class="form-control" disabled>
-                <option value="">Select District</option>
+                <option value="">選擇鄉鎮市區</option>
             </select>
         </div>
 
         <div class="mb-4">
-            <label for="road" class="form-label">Road:</label>
+            <label for="road" class="form-label">路口：</label>
             <input type="text" id="road" name="road" class="form-control">
             <select id="road-results" class="form-control mt-2" size="5" style="display: none;"></select>
         </div>
