ab32d1458130f4dccaf175ea8dfb4d7f880d4c37
bency <bency80097@gmail.com>
2024-06-04 17:42:24 +0800

    feat: refactor profile view and enhance Vite server security
    - Remove the update password form from the profile edit view
    - Add HTTPS support to the Vite server configuration with specific key and certificate files
    - Change the host and public URLs in the Vite server configuration
    
    Signed-off-by: bency <bency80097@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import fs from "fs";
+import path from "path";
 
 export default defineConfig({
     server: {
-        host: "0.0.0.0",
+        https: {
+            key: fs.readFileSync(
+                path.resolve(__dirname, "certs/traffic.keeping.work.key")
+            ),
+            cert: fs.readFileSync(
+                path.resolve(__dirname, "certs/traffic.keeping.work.crt")
+            ),
+        },
+        host: "localhost.keeping.work",
         port: 5173,
+        public: "https://localhost.keeping.work:5173",
     },
     plugins: [
         laravel({
